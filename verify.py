8b7f1afab14cd79bfe92019f3831e933a794e9b0
Hurshal Patel <hpatel516@gmail.com>
2015-10-19 20:24:48 -0700

    verify.py is now social mobile local hipsterbro fuck you

--- verify.py
+++ verify.py
@@ -1,24 +1,55 @@
-#!/usr/local/bin/python
+#!/usr/bin/env python
 
+import os
+import sys
 import yaml
+import argparse
 
 SCHEMA = {
-        'required': {'title': str, 'desc': str},
-        'optional': {'details': list}
-        }
+    'required': {
+        'title': str,
+        'desc': str
+    },
+    'optional': {
+        'details': list
+    }
+}
 
-def verify_schema(rows):
-    for row in rows:
+def find_schema_errors(rows):
+    errors = False
+    for i, row in enumerate(rows):
         for field_name, field_type in SCHEMA['required'].iteritems():
-            assert isinstance(row[field_name], field_type)
+            if not isinstance(row[field_name], field_type):
+                print("Row %d: %s is not %s" % (i, field_name, field_type))
         for field_name, field_type in SCHEMA['optional'].iteritems():
-            if field_name in row:
-                assert isinstance(row[field_name], field_type)
+            if field_name in row and not isinstance(row[field_name], field_type):
+                errors = True
+                print("Row %d: %s is not %s" % (i, field_name, field_type))
+
+    return errors
 
 if __name__ == '__main__':
-    with open('list.yaml', 'r') as f:
-        y = yaml.load(f)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', default='list.yaml', help='The file to verify')
 
-    print "lets see here..."
-    verify_schema(y)
-    print "you sir, are not a fucking idiot"
+    options = parser.parse_args()
+    if not os.path.exists(options.file):
+        print("You suck %s doesn't exist" % options.file)
+        sys.exit(1)
+
+    try:
+        with open(options.file, 'r') as f:
+            rows = yaml.load(f)
+            print("lets see here...")
+            if find_schema_errors(rows):
+                print("You sir, are a fucking idiot. Exiting.")
+                sys.exit(4)
+            else:
+                print("You sir, are not a fucking idiot.")
+                sys.exit(0)
+    except IOError:
+        print("You suck %s is not for plebs" % options.file)
+        sys.exit(2)
+    except Exception as e:
+        print("You sir, are a fucking idiot: %s" % e)
+        sys.exit(3)
